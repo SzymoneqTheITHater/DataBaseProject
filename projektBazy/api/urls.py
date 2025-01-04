f544cb6ce1c32c6d9ac21e78e30b9312d39f3621
SzymoneqTheITHater <szymon.jaslar.2.0@gmail.com>
2025-01-04 01:20:04 +0100

    Przekopiowanie na piechotę z add-transactions-adding-chat

--- projektBazy/api/urls.py
+++ projektBazy/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListingsView, UserAddressesView, CreateListingView
+from .views import ListingsView, UserAddressesView, CreateListingView, ReviewViewSet
 from . import views
 urlpatterns = [
     path('',views.getData),
@@ -16,11 +16,20 @@ urlpatterns = [
     path('listings/user/<int:user_id>/', ListingsView.as_view(), name='user-listings'),   
     path('listings/category/<int:category_id>/', ListingsView.as_view(), name='category-listings'),
     path('listing/<int:listing_id>/cancel/', views.cancel_listing, name='cancel_listing'),
+    path('listings/<int:listing_id>/', views.get_listing, name='listing'),
     path('user/<int:user_id>/addresses/', UserAddressesView.as_view(), name='user-addresses'),
     path('transactions/', views.create_transaction, name='create-transaction'),
     path('transactions/<int:transaction_id>/update/', views.update_transaction_status, name='update-transaction-status'),
     path('listings/create/', CreateListingView.as_view(), name='create-listing'),
-    
+    path('addmessage/<int:listing_id>/<int:chat_id>', views.add_chat_message, name='add-chat-message'),
+    path('addmessage/<int:listing_id>', views.add_chat_message, name='add-chat-message'),
+    path('chats/<int:listing_id>', views.get_chats, name='listing-chats'),
+    path('messages/<int:chat_id>', views.get_messages, name='chat-messages'),
+    path('viewedmessage/<int:message_id>', views.set_message_viewed, name='message-viewed'),
+    path('reviews/', ReviewViewSet.as_view({'get': 'list', 'post': 'create'}), name='review-list-create'),
+    path('reviews/<int:pk>/', ReviewViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='review-detail'),
+    path('reviews/<int:pk>/user-reviews/', ReviewViewSet.as_view({'get': 'user_reviews'}), name='user-reviews'),
+    path('reviews/<int:pk>/average-rating/', ReviewViewSet.as_view({'get': 'average_rating'}), name='average-rating'),
 ]
 #Do something like this to test it
 #http://127.0.0.1:8000/postAddress/
--- projektBazy/api/views.py
+++ projektBazy/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from shop.models import Category, Listing, Address, Transaction
+from shop.models import Category, Listing, Address, Transaction, Message, Chat, Review
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CategorySerializer, ListingSerializer, UserSerializer, AdressSerializer, TransactionSerializer
+from .serializers import CategorySerializer, ListingSerializer, UserSerializer, AdressSerializer, TransactionSerializer, MessageSerializer, ChatSerializer, ReviewSerializer
 from rest_framework import status, generics
 from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import NotFound
@@ -14,6 +14,10 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from django.db.models import Avg
+from datetime import datetime
 
 class ListingPagination(PageNumberPagination):
     page_size = 16  
@@ -274,6 +278,141 @@ def cancel_listing(request, listing_id):
     serializer = ListingSerializer(listing)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def add_chat_message(request, chat_id, listing_id):
+    if request.method == 'POST':
+        try:
+            listing = Listing.objects.get(id=listing_id)
+        except Listing.DoesNotExist:
+            return Response({'detail': 'Listing not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        try:
+            chat = Chat.objects.get(id=chat_id)
+            if chat.listing_id != listing.id:
+                return Response({'detail': 'Invalid chat for the listing'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Chat.DoesNotExist:
+            chat = None
+                        
+        if chat is None:
+            
+            if request.user.id == listing.seller.id:
+                return Response({'detail': 'Seller can not start the chat'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                newChat = {
+                    'buyer': request.user,
+                    'seller': listing.seller,
+                    'listing': listing
+                }
+                chat_serializer = ChatSerializer(data=newChat, context={'request': request})
+                if chat_serializer.is_valid():
+                   chat = chat_serializer.save(seller=listing.seller, buyer=request.user, listing=listing)
+            
+            except Exception as e:
+                return Response({'detail': 'Unable to create chat'}, status=status.HTTP_409_CONFLICT)
+
+        new_message = {
+            'content' : request.data.get('message')
+        }
+
+        message_serializer = MessageSerializer(data=new_message, context={'request': request})
+
+        if message_serializer.is_valid():
+            message_serializer.save(chat=chat)
+            return Response(message_serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(message_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_chats(request, listing_id):
+    try:
+        listing = Listing.objects.get(id=listing_id)
+    except Listing.DoesNotExist:
+        return Response({'detail': 'Listing not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    chats = Chat.objects.filter(listing=listing_id)
+    serializer = ChatSerializer(chats, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_messages(request, chat_id):
+    try:
+        chat = Chat.objects.get(id=chat_id)
+    except Chat.DoesNotExist:
+        return Response({'detail': 'Chat not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    messages = Message.objects.filter(chat=chat)
+    serializer = MessageSerializer(messages, many=True)
+    return Response(serializer.data)
+
+@api_view(['PATCH'])
+def set_message_viewed(request, message_id):
+    try:
+        message = Message.objects.get(id=message_id)
+        message.status = Message.Viewed
+        message.viewed_at = datetime.now()
+        message.save()
+    except Message.DoesNotExist:
+        return Response({'detail': 'Message not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'detail': 'Unable to update'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return Response(MessageSerializer(message).data)    
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def create(self, request, *args, **kwargs):
+        reviewer = request.user
+        listing_id = request.data.get('listing')
+        reviewee_id = request.data.get('reviewee')
+
+        try:
+            listing = Listing.objects.get(id=listing_id)
+            if listing.seller == reviewer:
+               return Response({"error": "You can only review users for listings you purchased."}, status=status.HTTP_403_FORBIDDEN)
+            if listing.seller.id != int(reviewee_id):
+                return Response({"error": "Reviewee must be the seller of the purchased listing."}, status=status.HTTP_400_BAD_REQUEST)
+        except Listing.DoesNotExist:
+            return Response({"error": "Listing does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+     #   if instance.reviewer != request.user:
+     #       return Response({"error": "You can only update your own reviews."}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    @action(detail=True, methods=['get'], url_path='user-reviews')
+    def user_reviews(self, request, pk=None):
+        user = User.objects.get(pk=pk)
+        reviews = Review.objects.filter(reviewee=user)
+        serializer = self.get_serializer(reviews, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'], url_path='average-rating')
+    def average_rating(self, request, pk=None):
+        user = User.objects.get(pk=pk)
+        average_rating = Review.objects.filter(reviewee=user).aggregate(Avg('rating'))['rating__avg']
+        return Response({"average_rating": average_rating})
+   
+   
+
+
+@api_view(['GET'])
+def get_listing(request, listing_id):
+    try:
+        listing = Listing.objects.get(id=listing_id)
+        serializer = ListingSerializer(listing)
+        return Response(serializer.data)
+    except Listing.DoesNotExist:
+        return Response({'detail': 'Listing not found'}, status=status.HTTP_404_NOT_FOUND)
+    
     
 
     
